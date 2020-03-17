86a1c179fd6ac8a5fd31ce9b1d477e4f25570286
Eunjin <>
2020-03-17 11:37:31 +0900

    Add RoundView to make corner in UIView

---
+++ ArcDraw/ArcDraw/RoundView.swift
@@ -0,0 +1,22 @@
+//
+//  RoundView.swift
+//  ArcDraw
+//
+//  Created by Eunjin on 17/03/2020.
+//  Copyright © 2020 Eunjin. All rights reserved.
+//
+
+import UIKit
+
+class RoundView: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat {
+        set(newValue) {
+            self.layer.cornerRadius = newValue
+            self.layer.masksToBounds = true
+            self.layer.maskedCorners = [.layerMaxXMinYCorner]
+        }
+        get {
+            return self.layer.cornerRadius
+        }
+    }
+}
